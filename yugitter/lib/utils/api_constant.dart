85383391ef8fa6ccaaf50f5093c12f98ceb41e5f
hermasyp <hermas.yuda@gmail.com>
2019-07-02 00:48:11 +0700

    changing api endpoint

--- yugitter/lib/utils/api_constant.dart
+++ yugitter/lib/utils/api_constant.dart
@@ -1,2 +1,2 @@
 
-const String BASE_YUGIOH_URL = 'https://db.ygoprodeck.com/api/v5/cardinfo.php?attribute=divine';
+const String BASE_YUGIOH_URL = 'https://db.ygoprodeck.com/api/v5/cardinfo.php?num=20';
